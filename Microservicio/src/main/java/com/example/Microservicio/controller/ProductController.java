79ca7ae3cc9ad21225275ffe4a1c1e9a1d3efed2
Joaquin Lepe <joa.lepe@duocuc.cl>
2025-11-10 16:03:43 -0300

    cambios pequeños

--- Microservicio/src/main/java/com/example/Microservicio/controller/ProductController.java
+++ Microservicio/src/main/java/com/example/Microservicio/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/api/productos")
 @Tag(name = "Manejo de los productos", description = "Operaciones para gestionar productos")
 public class ProductController {
     
@@ -18,25 +18,25 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    @Operation(summary = "Ver lista de libros disponibles", description = "Obtiene todos los libros almacenados")
+    @Operation(summary = "Ver lista de productos disponibles", description = "Obtiene todos los productos almacenados")
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
  
     @GetMapping("/{id}")
-    @Operation(summary = "Obtener libro por ID", description = "Devuelve un libro según su identificador")
+    @Operation(summary = "Obtener productos por ID", description = "Devuelve un productos según su identificador")
     public Product getBookById(@PathVariable Long id) {
         return productService.getProductById(id);
     }
 
     @PostMapping
-    @Operation(summary = "Agregar un nuevo libro", description = "Crea un nuevo registro de libro")
+    @Operation(summary = "Agregar un nuevo productos", description = "Crea un nuevo registro de productos")
     public Product createBook(@RequestBody Product product) {
         return productService.saveProduct(product);
     }
  
     @PutMapping("/{id}")
-    @Operation(summary = "Actualizar un libro existente", description = "Modifica los datos de un libro según su ID")
+    @Operation(summary = "Actualizar un productos existente", description = "Modifica los datos de un productos según su ID")
     public Product updateBook(@PathVariable Long id, @RequestBody Product product) {
         Product existingProduct = productService.getProductById(id);
         if (existingProduct != null) {
@@ -49,7 +49,7 @@ public class ProductController {
     }
  
     @DeleteMapping("/{id}")
-    @Operation(summary = "Eliminar un libro", description = "Elimina un libro por su identificador")
+    @Operation(summary = "Eliminar un productos", description = "Elimina un productos por su identificador")
         public void deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
     }
